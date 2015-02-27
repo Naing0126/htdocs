bf0665965037ef9d6af3b0584ea318030d7819d0
Naing <na0126dud@gmail.com>
2015-02-28 01:53:51 +0900

    Revert "initial commit"
    This reverts commit 524bb5864c264537af77f6899ea8f6a229f6e0ac.

--- application/views/frame.php
+++
@@ -1,2 +0,0 @@
-  <iframe class="iframe" name="board"  frameborder="0" src="<?php echo site_url("dashboard/directory_list/".$uid."");?>">
- </iframe>
--- application/views/get_directory_name.php
+++
@@ -1,6 +0,0 @@
-<article>
-    <h1><?=$directory->directory_uid?></h1>
-    <div>
-        <?=$directory->directory_name?>
-    </div>
-</article>
