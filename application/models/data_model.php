026bb1b4afe11d18442501e651c7cefe3b792b4b
Naing <na0126dud@gmail.com>
2015-02-28 17:50:03 +0900

    chart date to string

--- application/models/data_model.php
+++ application/models/data_model.php
@@ -31,7 +31,7 @@ public function getDate($widgets_sid){
     if($query->num_rows() > 0){
       $data['cnt'][$sid] = $query->num_rows;
       foreach($query->result() as $v){
-        $data[$sid][] = $v->data_value;
+        $data[$sid][] = ""+$v->data_date;
       }
       return $data;
     }
